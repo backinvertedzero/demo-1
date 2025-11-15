dbd07910d2b09460824c90d518de9ed2de6c574c
InvertedInfinity <myinvertedinfinity@gmail.com>
2025-11-15 20:02:18 +0300

    fix route

--- src/Modules/routes.php
+++ src/Modules/routes.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\HuntingBooking\Http\Controllers\HuntingBookingController;
 
-Route::prefix('hunting-booking')->group(function () {
+Route::prefix('bookings')->group(function () {
     Route::post('/', [HuntingBookingController::class, 'create']);
 });
 
